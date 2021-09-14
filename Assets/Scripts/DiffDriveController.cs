a5c6529589cabe0f7fad82223e879bba51552e72
maopotcha <maopotcha@gmail.com>
2021-09-14 16:01:14 +0900

    DiffDriveControllerにてROSからのtwistメッセージを受け取って両輪速度を決定して出力する機能を追加し、ic120へも実装

--- Assets/Scripts/DiffDriveController.cs
+++ Assets/Scripts/DiffDriveController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Control;
+using RosMessageTypes.Geometry;
 
 public class DiffDriveController : MonoBehaviour
 {
+    private ROSConnection ros;
+
     public List<GameObject> leftWheels;
     public List<GameObject> rightWheels;
     public KeyCode leftUpKey = KeyCode.U;
@@ -11,19 +16,23 @@ public class DiffDriveController : MonoBehaviour
     public KeyCode rightUpKey = KeyCode.I;
     public KeyCode rightDownKey = KeyCode.K;
 
+    public string TwistTopicName = "zx120/twist";
     private List<ArticulationBody> leftBodies;
     private List<ArticulationBody> rightBodies;
+    private TwistMsg twist;
+    private double tread_half = 2.0;
 
     // Start is called before the first frame update
     void Start()
     {
+        ros = ROSConnection.instance;
         leftBodies = new List<ArticulationBody>();
         rightBodies = new List<ArticulationBody>();
         foreach (GameObject left in leftWheels)
         {
             var body = left.GetComponent<ArticulationBody>();
             var drive = body.xDrive;
-            body.mass = 100.0f;
+            body.mass = 50.0f;
             drive.stiffness = 0;
             drive.damping = 100000;
             drive.forceLimit = 100000;
@@ -34,13 +43,17 @@ public class DiffDriveController : MonoBehaviour
         {
             var body = right.GetComponent<ArticulationBody>();
             var drive = body.xDrive;
-            body.mass = 100.0f;
+            body.mass = 50.0f;
             drive.stiffness = 0;
             drive.damping = 100000;
             drive.forceLimit = 100000;
             body.xDrive = drive;
             rightBodies.Add(body);
         }
+        tread_half = Mathf.Abs(leftWheels[0].transform.localPosition.x - rightWheels[0].transform.localPosition.x)/2;
+
+        Debug.Log("DiffDriveController starts!!");
+        ros.Subscribe<TwistMsg>(TwistTopicName, ExecuteTwist);
     }
 
     // Update is called once per frame
@@ -48,6 +61,8 @@ public class DiffDriveController : MonoBehaviour
     {
         const float speed = 100.0f;
         float leftVel = 0;
+        float rightVel = 0;
+        /*
         if (Input.GetKey(leftUpKey))
         {
             leftVel = speed;
@@ -55,7 +70,7 @@ public class DiffDriveController : MonoBehaviour
         {
             leftVel = -speed;
         }
-        float rightVel = 0;
+
         if (Input.GetKey(rightUpKey))
         {
             rightVel = speed;
@@ -64,6 +79,11 @@ public class DiffDriveController : MonoBehaviour
         {
             rightVel = -speed;
         }
+        */
+
+        rightVel = (float)(twist.linear.x + tread_half * twist.angular.z) * Mathf.Rad2Deg;
+        leftVel = (float)(twist.linear.x - tread_half * twist.angular.z) * Mathf.Rad2Deg;
+
         foreach (ArticulationBody left in leftBodies)
         {
             var drive = left.xDrive;
@@ -76,5 +96,13 @@ public class DiffDriveController : MonoBehaviour
             drive.targetVelocity = rightVel;
             right.xDrive = drive;
         }
+    }
+
+    void ExecuteTwist(TwistMsg msg)
+    {
+        //currentPose = trajectory;
+        twist = msg;
+        Debug.Log("Linear Velocity:"+twist.linear.x);
+        Debug.Log("Angular Velocity:"+twist.angular.z);
     }
 }
