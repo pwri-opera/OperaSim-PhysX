2ef62f60baa337e9e219cc811b8a1afd6901b61e
maopotcha <maopotcha@gmail.com>
2021-09-28 08:37:41 +0900

    c30-rのurdfファイルを修正した-refineを作成しsceneへインポート

--- Assets/Scripts/DiffDriveController.cs
+++ Assets/Scripts/DiffDriveController.cs
@@ -63,7 +63,6 @@ public class DiffDriveController : MonoBehaviour
         float leftVel = 0;
         float rightVel = 0;
         
-        /*
         if (Input.GetKey(leftUpKey))
         {
             leftVel = speed;
@@ -80,11 +79,12 @@ public class DiffDriveController : MonoBehaviour
         {
             rightVel = -speed;
         }
-        */
         
+        
+        /*
         rightVel = (float)(twist.linear.x + tread_half * twist.angular.z) * Mathf.Rad2Deg;
         leftVel = (float)(twist.linear.x - tread_half * twist.angular.z) * Mathf.Rad2Deg;
-        
+        */
         foreach (ArticulationBody left in leftBodies)
         {
             var drive = left.xDrive;
