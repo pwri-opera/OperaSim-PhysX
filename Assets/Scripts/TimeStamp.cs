26922d38cb21c50c0e09dd346bf7d0c5e745a28c
Ren Komatsu <komatsu@robot.t.u-tokyo.ac.jp>
2023-03-07 17:51:58 +0900

    Add ROS1/ROS2 compatible timestamp

--- Assets/Scripts/TimeStamp.cs
+++ Assets/Scripts/TimeStamp.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 using RosMessageTypes.BuiltinInterfaces;
 // From: https://github.com/Unity-Technologies/Robotics-Nav2-SLAM-Example/tree/main/Nav2SLAMExampleProject/Assets/Scripts
 
+
+// Type for Seconds is different btw ROS1 and ROS2
+#if ROS2
+using RosSecsType = System.Int32;
+#else
+using RosSecsType = System.UInt32;
+#endif
+
 namespace Unity.Robotics.Core
 {
     public readonly struct TimeStamp
@@ -10,7 +18,7 @@ namespace Unity.Robotics.Core
         public const double k_NanosecondsInSecond = 1e9f;
 
         // TODO: specify base time this stamp is measured against (Sim 0, time since application start, etc.)
-        public readonly uint Seconds;
+        public readonly RosSecsType Seconds;
         public readonly uint NanoSeconds;
 
         // (From Unity Time.time)
@@ -19,19 +27,12 @@ namespace Unity.Robotics.Core
             var sec = Math.Floor(timeInSeconds);
             var nsec = (timeInSeconds - sec) * k_NanosecondsInSecond;
             // TODO: Check for negatives to ensure safe cast
-            Seconds = (uint)sec;
+            Seconds = (RosSecsType)sec;
             NanoSeconds = (uint)nsec;
         }
 
-        //// (From a ROS2 Time message)
-        //TimeStamp(int sec, uint nsec)
-        //{
-        //    Seconds = sec;
-        //    NanoSeconds = nsec;
-        //}
-
-        // (From a ROS1 Time message)
-        TimeStamp(uint sec, uint nsec)
+        // (From a ROS2 or ROS1 Time message)
+        TimeStamp(RosSecsType sec, uint nsec)
         {
             Seconds = sec;
             NanoSeconds = nsec;
