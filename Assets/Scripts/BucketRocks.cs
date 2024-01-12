4095b174de05ba8f7376588e4886eeaa5714b1f7
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2024-01-12 12:26:10 +0900

    keep the particle state when the particle is still moveing (ref #10)

--- Assets/Scripts/BucketRocks.cs
+++ Assets/Scripts/BucketRocks.cs
@@ -7,14 +7,28 @@ public class RockObjectDetector : MonoBehaviour
 {
 	public GameObject manager;
 	public GameObject terrain;
+	private bool touchground = false;
 
     private void OnCollisionEnter(Collision collision)
     {
 		if (collision.gameObject == terrain)
         {
-			manager.SendMessage("OnRockTerrainCollision", this.gameObject);
+			touchground = true;
 		}
 	}
+
+    private void FixedUpdate()
+    {
+        if (touchground)
+		{
+            var rigidbody = GetComponent<Rigidbody>();
+            var velocity = rigidbody.velocity.sqrMagnitude;
+            if (velocity < 0.1)
+            {
+                manager.SendMessage("OnRockTerrainCollision", this.gameObject);
+            }
+        }
+    }
 }
 
 public class BucketRocks : MonoBehaviour
