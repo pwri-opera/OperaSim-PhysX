92460fd3c5b9a7d96b131d3778ed7ebbc580ee09
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2024-09-20 10:44:12 +0900

    refactor particle management function to SoilParticleSettings.cs

--- Assets/Scripts/SoilParticleSettings.cs
+++ Assets/Scripts/SoilParticleSettings.cs
@@ -2,6 +2,61 @@
 using UnityEditor;
 using System.Collections.Generic;
 using System;
+using GK;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Burst;
+
+/// <summary>
+/// 各粒子にアタッチしてTerrainとの衝突イベントを通知するユーティリティスクリプト
+/// </summary>
+public class RockObjectDetector : MonoBehaviour
+{
+
+    [Tooltip("衝突イベントを通知する先のマネージャクラス")]
+    public SoilParticleSettings manager;
+    private double timecreated = 0.0;
+    private Vector3 pos_last_collision = Vector3.zero;
+
+    private void Start()
+    {
+        timecreated = Time.timeAsDouble;
+        pos_last_collision = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+        {
+            pos_last_collision = transform.position;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+        {
+            pos_last_collision = transform.position;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Time.timeAsDouble - timecreated > 1.5)
+        {
+            var rigidbody = GetComponent<Rigidbody>();
+            var velocity = rigidbody.velocity.sqrMagnitude;
+            if (velocity < 0.2 && Vector3.Distance(transform.position, pos_last_collision) < 0.1)
+            {
+                manager.OnRockTerrainCollision(this.gameObject);
+            }
+        }
+        if (this.transform.position.y - pos_last_collision.y < -1.0)
+        {
+            manager.OnRockTerrainCollision(this.gameObject);
+        }
+    }
+}
 
 /// <summary>
 /// 掘削時の地形変形パラメータの設定
@@ -33,8 +88,23 @@ public class SoilParticleSettings : MonoBehaviour
     [Tooltip("heightmapを同期する間隔(秒)")]
     public float syncPeriod = 1.0f;
 
+    [Tooltip("生成する粒子のPefabを設定してください")]
+    public GameObject rockPrefab;
+
+    private List<GameObject> rocks;
+    private ConvexHullCalculator calc;
+    private float particle_volume;
+    private List<Mesh> mesh_patterns;
+    private double last_created_time = 0.0;
+
+    private ParticleForceJob job;
+    private JobHandle job_handle;
+    private bool job_started = false;
+    private NativeArray<float3> job_positions;
+    private NativeArray<float3> job_forces;
+
     private float timeElapsed = 0.0f;
-    
+
     ComputeShader cs = null;
     int thermalKernelIdx = -1;
     int xRes = -1;
@@ -74,6 +144,155 @@ public class SoilParticleSettings : MonoBehaviour
             throw new MissingReferenceException("Could not find compute shader fo digging");
         }
         digKernelIdx = cs2.FindKernel("Dig");
+
+        // pregenerate particle meshes
+        calc = new ConvexHullCalculator();
+        rocks = new List<GameObject>();
+        particle_volume = (float)(4.0 / 3.0 * Math.PI * Math.Pow(particleVisualRadius, 3));
+        mesh_patterns = new List<Mesh>();
+        for (int i = 0; i < 10; i++)
+        {
+            var verts = new List<Vector3>();
+            var tris = new List<int>();
+            var normals = new List<Vector3>();
+            var points = new List<Vector3>();
+
+            points.Clear();
+
+            for (int j = 0; j < 100; j++)
+            {
+                points.Add(UnityEngine.Random.insideUnitSphere * particleVisualRadius);
+            }
+
+            calc.GenerateHull(points, true, ref verts, ref tris, ref normals);
+
+            var mesh = new Mesh();
+            mesh.SetVertices(verts);
+            mesh.SetTriangles(tris, 0);
+            mesh.SetNormals(normals);
+
+            mesh_patterns.Add(mesh);
+        }
+
+        last_created_time = Time.timeAsDouble;
+    }
+
+    private void CreateRock(Vector3 point)
+    {
+        var rock = Instantiate(rockPrefab);
+
+        rock.transform.parent = null;
+        rock.transform.position = point;
+        rock.AddComponent<RockObjectDetector>();
+        rock.GetComponent<RockObjectDetector>().manager = this;
+        rock.GetComponent<MeshFilter>().sharedMesh = mesh_patterns[UnityEngine.Random.Range(0, mesh_patterns.Count)];
+
+        rocks.Add(rock);
+    }
+
+    public void OnBucketCollision(Collision other)
+    {
+        if (!enable)
+            return;
+
+        if (other.gameObject.name != "Terrain")
+            return;
+
+        var now = Time.timeAsDouble;
+        if (now - last_created_time > 0.01)
+        {
+            var point = other.GetContact(0).point;
+            ModifyTerrain(point, -particle_volume);
+            CreateRock(point);
+            last_created_time = now;
+        }
+    }
+
+    public void OnRockTerrainCollision(GameObject rock)
+    {
+        if (Vector3.Distance(transform.position, rock.transform.position) > 2.0)
+        {
+            ModifyTerrain(rock.transform.position, particle_volume);
+            Destroy(rock);
+            rocks.Remove(rock);
+        }
+    }
+
+    [BurstCompile]
+    private struct ParticleForceJob : IJobParallelFor
+    {
+        [ReadOnly]
+        public float particleStickDistance;
+        [ReadOnly]
+        public NativeArray<float3> positions;
+        [WriteOnly]
+        public NativeArray<float3> forces;
+
+        public void Execute(int index)
+        {
+            var rock1 = positions[index];
+            var repulvector = new float3(0, 0, 0);
+            for (var j = 0; j < positions.Length; j++)
+            {
+                var rock2 = positions[j];
+                float dist = math.distance(rock1, rock2);
+                if (dist < particleStickDistance)
+                {
+                    repulvector += rock1 - rock2;
+                }
+            }
+            forces[index] = math.normalize(repulvector);
+        }
+    }
+
+    private void Destroy()
+    {
+        if (job_started)
+        {
+            job_handle.Complete();
+            job_positions.Dispose();
+            job_forces.Dispose();
+        }
+    }
+
+    // Update is called once per frame
+    void UpdateStickForce()
+    {
+        if (job_started)
+        {
+            job_handle.Complete();
+            if (rocks.Count == job.forces.Length)
+            {
+                for (var i = 0; i < rocks.Count; i++)
+                {
+                    var rock1 = rocks[i];
+                    var f = job.forces[i];
+                    if (!float.IsNaN(f.x))
+                    {
+                        rock1.GetComponent<Rigidbody>().AddForce(-f * SoilParticleSettings.instance.stickForce);
+                    }
+                }
+            }
+            job_positions.Dispose();
+            job_forces.Dispose();
+        }
+
+        job_positions = new NativeArray<float3>(rocks.Count, Allocator.Persistent);
+        job_forces = new NativeArray<float3>(rocks.Count, Allocator.Persistent);
+
+        for (var i = 0; i < rocks.Count; i++)
+        {
+            var rock1 = rocks[i];
+            job_positions[i] = rock1.transform.position;
+        }
+
+        job = new ParticleForceJob();
+        job.particleStickDistance = (float)SoilParticleSettings.instance.partileStickDistance;
+        job.positions = job_positions;
+        job.forces = job_forces;
+
+        job_handle = job.Schedule(job_forces.Length, 1);
+        job_started = true;
     }
 
     private void Start()
@@ -127,28 +346,27 @@ public class SoilParticleSettings : MonoBehaviour
         gameObject.GetComponent<Terrain>().terrainData.SetHeights(0, 0, originalHeights);
     }
 
-    public static void ModifyTerrain(Vector3 point, float diff)
+    public void ModifyTerrain(Vector3 point, float diff)
     {
-        if (instance == null)
-            return;
-
-        if (!instance.enable)
+        if (!enable)
             return;
 
         RenderTexture prevRT = RenderTexture.active;
 
-        var terrainData = instance.GetComponent<Terrain>().terrainData;
-        Vector3 relpos = (point - instance.transform.position);
+        var terrainData = GetComponent<Terrain>().terrainData;
+        Vector3 relpos = (point - transform.position);
         var posXInTerrain = (int)(relpos.x / terrainData.size.x * instance.xRes);
         var posYInTerrain = (int)(relpos.z / terrainData.size.z * instance.yRes);
 
-        instance.cs2.SetTexture(instance.digKernelIdx, "heightmap", instance.heightmapRT0);
-        instance.cs2.SetFloat("diff", diff);
-        instance.cs2.SetVector("pos", new Vector2(posXInTerrain, posYInTerrain));
+        cs2.SetTexture(digKernelIdx, "heightmap", heightmapRT0);
+        cs2.SetFloat("diff", diff);
+        cs2.SetVector("pos", new Vector2(posXInTerrain, posYInTerrain));
 
-        instance.cs2.Dispatch(instance.digKernelIdx, 1, 1, 1);
+        cs2.Dispatch(digKernelIdx, 1, 1, 1);
 
-        RenderTexture.active = instance.heightmapRT0;
+        RenderTexture.active = heightmapRT0;
+
+        // TODO: need to modifiy for tiled heightmap
         RectInt rect = new RectInt(posXInTerrain - 10, posYInTerrain - 10, 20, 20);
         terrainData.CopyActiveRenderTextureToHeightmap(rect, rect.min, TerrainHeightmapSyncControl.None);
 
@@ -163,6 +381,8 @@ public class SoilParticleSettings : MonoBehaviour
 
         if (!enable)
             return;
+
+        UpdateStickForce();
 
         RenderTexture prevRT = RenderTexture.active;
 
@@ -199,6 +419,8 @@ public class SoilParticleSettings : MonoBehaviour
                     {
                         base_link = robot.GetComponent(typeof(Component));
                     }
+
+                    // TODO: need to modify for tiled heightmap
                     Vector3 relpos = (base_link.transform.position - gameObject.transform.position);
                     var posXInTerrain = (int)(relpos.x / terrainData.size.x * xRes);
                     var posYInTerrain = (int)(relpos.z / terrainData.size.z * yRes);
@@ -209,6 +431,7 @@ public class SoilParticleSettings : MonoBehaviour
 
             timeElapsed = 0.0f;
         }
+        // TODO: need to modify for tiled heightmap
         terrainData.SyncHeightmap();
 
         // swap
