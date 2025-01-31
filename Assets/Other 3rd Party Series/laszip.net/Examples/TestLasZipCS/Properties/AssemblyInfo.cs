92025f62fc919a25e7caf77780148792eb7de68e
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2025-01-31 17:32:33 +0900

    import laszip.net library

---
+++ Assets/Other 3rd Party Series/laszip.net/Examples/TestLasZipCS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestLasZipCS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("laszip.net")]
+[assembly: AssemblyCopyright("Copyright of C# port © 2014-2017 by Shinta <shintadono@googlemail.com>\r\nCopyright © 2005-2014, martin isenburg <martin.isenburg@rapidlasso.com>, rapidlasso - tools to catch reality <http:\\rapidlasso.com>")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("feb0bc3d-f0ec-4f1f-84b8-6a0ca6519b01")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
